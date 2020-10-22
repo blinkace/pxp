701a171201f912b858fccd92449cf888825233b0
Paul Warren <pdw@ex-parrot.com>
2020-10-22 22:48:19 +0100

    Convert paths to file: URLs

--- loadxbrl.py
+++ loadxbrl.py
@@ -6,6 +6,8 @@ import os
 import os.path
 import argparse
 from xbrl.json.report import JSONSerialiser
+from urllib.parse import urlparse
+from urllib.request import pathname2url
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -27,8 +29,15 @@ if args.packages is not None:
         processor.addTaxonomyPackage(tp)
 
 try:
-    #report = processor.loadXBRLReport(args.reports[0])
-    report = processor.loadIXBRLReport(args.reports[0])
+    src = args.reports[0]
+
+    # Convert paths to URLs, if required.
+    # This probably won't work with Windows-style c:\ URLs
+    url = urlparse(src)
+    if url.scheme == '':
+        src = "file:%s" % pathname2url(src)
+    
+    report = processor.loadIXBRLReport(src)
     js = JSONSerialiser()
     print(js.serialise(report))
 
