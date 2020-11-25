e337809eba149c4335d2b751a6fec3ef2f9d6e45
Paul Warren <pdw@ex-parrot.com>
2020-11-25 17:56:42 +0000

    Fix NCName regex.

--- xbrl/common/regex.py
+++ xbrl/common/regex.py
@@ -1,6 +1,6 @@
 import re
 
-namestartchar = r'[:A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\U00010000-\U000EFFFF]'
+namestartchar = r'[A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\U00010000-\U000EFFFF]'
 namechar = r'(?:' + namestartchar + r'|[-.0-9\xB7\u0300-\u036F\u203F-\u2040])'
 ncname = namestartchar + namechar + '*'
 qname = ncname + ':' + ncname
