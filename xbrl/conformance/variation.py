672f11916caabaafe2136e012f77e1b2369f32a6
Paul Warren <pdw@ex-parrot.com>
2020-12-01 18:17:01 +0000

    Fix conformance runner.

--- xbrl/conformance/variation.py
+++ xbrl/conformance/variation.py
@@ -1,8 +1,8 @@
 class Variation:
 
-    def __init__(self, vid, name, description, inputs = [], errors = {}):
+    def __init__(self, vid, name, description, inputs = None, errors = None):
         self.id = vid
         self.name = name
         self.description = description
-        self.inputs = inputs
-        self.errors = errors
+        self.inputs = inputs if inputs is not None else []
+        self.errors = errors if errors is not None else {}
