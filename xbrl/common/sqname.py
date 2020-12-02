6e5dadc8cb1fb158e189e0058efb44767711cf8d
Paul Warren <pdw@ex-parrot.com>
2020-12-02 20:06:37 +0000

    SQName parser.

---
+++ xbrl/common/sqname.py
@@ -0,0 +1,20 @@
+from xbrl.xbrlerror import XBRLError
+from .regex import RE_NCNAME
+
+class InvalidSQName(Exception):
+    pass
+
+def parseSQName(sqname, nsmap):
+
+    if ":" not in sqname:
+        raise InvalidSQName("%s is not a valid SQName" % sqname)
+
+    (prefix, localname) = sqname.split(":", 2)
+    if RE_NCNAME.match(prefix) is None:
+        raise InvalidSQName("%s is not a valid SQName (invalid prefix)" % sqname)
+
+    uri = nsmap.get(prefix)
+    if uri is None:
+        raise XBRLError("oimce:unboundPrefix", "Unknown prefix '%s'" % prefix)
+    return uri, localname
+
