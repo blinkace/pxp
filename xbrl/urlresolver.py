efd10df2e6cdd5c8980ed4207e33d9ee31885e60
Paul Warren <pdw@ex-parrot.com>
2021-01-05 18:09:22 +0000

    Improve handling of HTTP errors.

--- xbrl/urlresolver.py
+++ xbrl/urlresolver.py
@@ -1,23 +1,32 @@
 import io
 import urllib.request
+import urllib.error
 import logging
+
+from .xbrlerror import XBRLError
+
+logger = logging.getLogger(__name__)
 
 class URLResolver:
 
     def __init__(self):
         self.packages = []
 
-
     def addPackage(self, package):
         self.packages.append(package)
-
 
     def open(self, url):
         for p in self.packages:
             if p.hasFile(url):
-                logging.debug("Opening %s from package" % url)
+                logger.debug("Opening %s from package" % url)
                 return p.open(url)
-        return urllib.request.urlopen(url)
+        try:
+            logger.debug("Opening %s" % url)
+            return urllib.request.urlopen(url)
+        except urllib.error.HTTPError as e:
+            raise XBRLError("pyxbrle:HTTPError", "Unable to open %s: %s %s" % (url, e.code, e.reason))
+
+
 
     
 
