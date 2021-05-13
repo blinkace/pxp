d92ad66decfc516d6073d1bd7e5e6da655f3cdde
Paul Warren <pdw@ex-parrot.com>
2021-05-13 17:25:39 +0100

    Export more.

--- xbrl/__init__.py
+++ xbrl/__init__.py
@@ -3,3 +3,4 @@ from .xbrlerror import XBRLError
 from .const import NS, PREFIX, NSMAP
 from .taxonomypackage import TaxonomyPackage
 from .taxonomypackageloader import TaxonomyPackageLoader
+from .common import ValidationResult, ValidationSeverity
