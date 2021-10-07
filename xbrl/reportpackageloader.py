42d7f7e22e38fe1f718cc9b32a838626b999b71a
Paul Warren <pdw@ex-parrot.com>
2021-10-07 17:09:04 +0100

    Additional report package checks.

--- xbrl/reportpackageloader.py
+++ xbrl/reportpackageloader.py
@@ -1,6 +1,7 @@
 import zipfile
 import re
 from .reportpackage import ReportPackage
+from xbrl.xbrlerror import XBRLError
 
 class ReportPackageLoader:
 
@@ -11,7 +12,12 @@ class ReportPackageLoader:
             if len(top) != 1:
                 raise XBRLError("tpe:invalidDirectoryStructure", "Multiple top-level directories")
             tld = top.pop()
+            if not any(f.startswith('%s/reports/' % tld) for f in contents):
+                raise XBRLError("pyxbrle:missingReportsDirectory", "Report package does not contain a 'reports' directory")
             reports = list(item for item in contents if re.match('%s/reports.*\.(html?|xhtml|xbrl)$' % tld, item) is not None)
+            if any(r.endswith('.xbrl') and r.count('/') > 1 for r in reports):
+                raise XBRLError("pyxbrle:misplacedXBRLFile", ".xbrl file found in subdirectory of reports directory")
+
         return ReportPackage(path, reports)
 
 
